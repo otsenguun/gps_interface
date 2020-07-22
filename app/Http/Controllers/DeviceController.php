c9d53e60a9d67512cdcec58bdc039d3aded8abc8
Tsenguun <tsenguunkoko@gmail.com>
2020-07-22 20:00:18 +0800

    gps fix

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -57,7 +57,7 @@ class DeviceController extends Controller
     public function show($id)
     {
         $device = Device::find($id);
-        $locations = Data::select('lat','lng')->where('imei',$device->imei)->whereNot('lat','0000.0000')->orderBy('id','desc')->limit(50)->get();
+        $locations = Data::where('imei',$device->imei)->whereNot('lat','0000.0000')->orderBy('id','desc')->limit(50)->get();
         $datas = Data::where('imei',$device->imei)->whereNot('lat','0000.0000')->orderBy('id','desc')->paginate(10);
 
         return view('pages.device.index',compact('datas','device','locations'));
