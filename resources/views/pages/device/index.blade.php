63ed94a1d71ba87e924ae63d32602ae9ff91c39e
Tsenguun <tsenguunkoko@gmail.com>
2020-07-22 19:01:42 +0800

    polylines

--- resources/views/pages/device/index.blade.php
+++ resources/views/pages/device/index.blade.php
@@ -19,42 +19,46 @@
       (function(exports) {
         "use strict";
 
-        // This example creates an interactive map which constructs a polyline based on
-        // user clicks. Note that the polyline only appears once its path property
-        // contains two LatLng coordinates.
-
+        // This example creates a 2-pixel-wide red polyline showing the path of
+        // the first trans-Pacific flight between Oakland, CA, and Brisbane,
+        // Australia which was made by Charles Kingsford Smith.
         function initMap() {
-          exports.map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 7,
+          var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 3,
             center: {
-              lat: 41.879,
-              lng: -87.624
-            } // Center the map on Chicago, USA.
+              lat: 0,
+              lng: -180
+            },
+            mapTypeId: "terrain"
           });
-          exports.poly = new google.maps.Polyline({
-            strokeColor: "#000000",
+          var flightPlanCoordinates = [
+            {
+              lat: 37.772,
+              lng: -122.214
+            },
+            {
+              lat: 21.291,
+              lng: -157.821
+            },
+            {
+              lat: -18.142,
+              lng: 178.431
+            },
+            {
+              lat: -27.467,
+              lng: 153.027
+            }
+          ];
+          var flightPath = new google.maps.Polyline({
+            path: flightPlanCoordinates,
+            geodesic: true,
+            strokeColor: "#FF0000",
             strokeOpacity: 1.0,
-            strokeWeight: 3
+            strokeWeight: 2
           });
-          exports.poly.setMap(exports.map); // Add a listener for the click event
-
-          exports.map.addListener("click", addLatLng);
-        } // Handles click events on a map, and adds a new point to the Polyline.
-
-        function addLatLng(event) {
-          var path = exports.poly.getPath(); // Because path is an MVCArray, we can simply append a new coordinate
-          // and it will automatically appear.
-
-          path.push(event.latLng); // Add a new marker at the new plotted point on the polyline.
-
-          var marker = new google.maps.Marker({
-            position: event.latLng,
-            title: "#" + path.getLength(),
-            map: exports.map
-          });
+          flightPath.setMap(map);
         }
 
-        exports.addLatLng = addLatLng;
         exports.initMap = initMap;
       })((this.window = this.window || {}));
     </script>
