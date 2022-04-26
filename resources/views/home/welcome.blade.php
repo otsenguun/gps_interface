3a7f7f781058f4c5bbeabc128089fcbf2a3fbadb
otsenguun <tsenguunkoko@gmail.com>
2022-04-26 15:40:24 +0800

    g analytc

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -194,5 +194,17 @@
     </div>
 
     <script src="{{asset('assets/dist/js/main.min.js')}}"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-227156442-1">
+    </script>
+    
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-227156442-1');
+    </script>
 </body>
 </html>
